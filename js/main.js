e5617923176633f5588a39ead18717f2446325bd
dogukanbasaran <dogukanbasarann@hotmail.com>
2025-01-29 16:43:47 +0300

    favorite list was added

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 const inputSearch = document.querySelector(".search-area input");
 const display = document.querySelector(".display-area");
+const favoriteList = document.querySelector(".favorite-list");
+const btnFavoriteList = document.querySelector(".btn-favorite-list");
+const favoriteMovies = document.querySelector(".favorite-movies-container");
 
 inputSearch.addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
@@ -44,9 +47,22 @@ const fetchData = async () => {
         genre.textContent = `Genre: ${movie.Type}`;
         infos.appendChild(genre);
 
+        const btnAddFavorite = document.createElement("button");
+        btnAddFavorite.classList.add("btn-add-favorite");
+        btnAddFavorite.innerHTML = `<i class="fa-solid fa-heart"></i>`;
+        btnAddFavorite.addEventListener("click", () => {
+          handleBtnAddFavorite(
+            movie.Poster,
+            movie.Title,
+            movie.Year,
+            movie.Type
+          );
+        });
+
         movieCard.appendChild(poster);
         movieCard.appendChild(title);
         movieCard.appendChild(infos);
+        movieCard.appendChild(btnAddFavorite);
         display.appendChild(movieCard);
       }
     } catch (error) {
@@ -79,3 +95,57 @@ const displayError = (msg) => {
     errorBox.remove();
   }, 3000);
 };
+
+const handleBtnAddFavorite = (poster, title, year, genre) => {
+  const newFavoriteMovie = {
+    favorite_poster: poster,
+    favorite_title: title,
+    favorite_year: year,
+    favorite_genre: genre,
+  };
+
+  console.log(newFavoriteMovie);
+
+  const favMovieCard = document.createElement("div");
+  favMovieCard.classList.add("movie-card");
+
+  const favPoster = document.createElement("img");
+  favPoster.classList.add("poster");
+  favPoster.src = `${newFavoriteMovie.favorite_poster}`;
+
+  const favTitle = document.createElement("span");
+  favTitle.classList.add("title");
+  favTitle.textContent = `${newFavoriteMovie.favorite_title}`;
+
+  const favInfos = document.createElement("div");
+  favInfos.classList.add("infos");
+
+  const favYear = document.createElement("span");
+  favYear.textContent = `Year: ${newFavoriteMovie.favorite_year}`;
+  favInfos.appendChild(favYear);
+
+  const favGenre = document.createElement("span");
+  favGenre.textContent = `Genre: ${newFavoriteMovie.favorite_genre}`;
+  favInfos.appendChild(favGenre);
+
+  favMovieCard.appendChild(favPoster);
+  favMovieCard.appendChild(favTitle);
+  favMovieCard.appendChild(favInfos);
+
+  favoriteMovies.appendChild(favMovieCard);
+  console.log(favMovieCard.innerHTML);
+};
+
+favoriteList.classList.add("opened-favorite-list");
+
+btnFavoriteList.addEventListener("click", () => {
+  favoriteList.classList.contains("opened-favorite-list")
+    ? favoriteList.classList.replace(
+        "opened-favorite-list",
+        "closed-favorite-list"
+      )
+    : favoriteList.classList.replace(
+        "closed-favorite-list",
+        "opened-favorite-list"
+      );
+});
